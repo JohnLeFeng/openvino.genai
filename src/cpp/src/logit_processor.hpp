JohnLeFeng/openvino.genai

commit de77f96933a4c012958059a108d55bf5e9acd0a8

Fixed logprobs for greedy. (#886)
Applied LogSoftmax to logits returned by _greedy_sample().
Ticket: CVS-152274

src/cpp/src/logit_processor.hpp
@@ -299,6 +299,20 @@ public:
     }
 };
 
+
+class LogFilter : public ILogitTransformer {
+public:
+    LogFilter() {}
+    
+    void apply(Logits& logits) override {
+        OPENVINO_ASSERT(logits.is_vector_initialized());
+        for (size_t i = 0; i < logits.m_size; i++) {
+            logits.m_vector[i].m_log_prob = std::log(logits.m_vector[i].m_log_prob);
+        }
+    }
+};
+
+
 } // namespace LogitTransformers
 
 class LogitProcessor {
@@ -352,6 +366,7 @@ public:
                 if (sampling_params.top_k > 0 && sampling_params.top_k < std::numeric_limits<size_t>::max()) {
                     m_logit_transformers.emplace_back(new LogitTransformers::TopKFilter(sampling_params.top_k));
                 }
+                m_logit_transformers.emplace_back(new LogitTransformers::LogFilter());
             }
         }
     }

src/cpp/src/sampler.hpp
@@ -358,7 +358,15 @@ class Sampler {
                 max_index = i;
             }
         }
-        return Token(logits.m_data[max_index], max_index);
+
+        // apply log softmax to max value
+        float log_sum = std::log(std::accumulate(
+            logits.m_data, logits.m_data + logits.m_size, 0.0f, [max_value](float accumulated, float to_add) {
+                return accumulated + std::exp(to_add - max_value);
+        }));
+        max_value = -log_sum;
+
+        return Token(max_value, max_index);
     }
 
     std::vector<Token> _multinomial_sample(const Logits& logits, size_t num_tokens_per_sequence) {

commit 31e6e0c86370924d5061d1562f9e60dd0de08d2e

Fixed regression in multinomial. (#889)
Fixed regression after
https://github.com/openvinotoolkit/openvino.genai/pull/886.
Turned on multinomial tests without ref comparison.

src/cpp/src/logit_processor.hpp
@@ -300,19 +300,6 @@ public:
 };
 
 
-class LogFilter : public ILogitTransformer {
-public:
-    LogFilter() {}
-    
-    void apply(Logits& logits) override {
-        OPENVINO_ASSERT(logits.is_vector_initialized());
-        for (size_t i = 0; i < logits.m_size; i++) {
-            logits.m_vector[i].m_log_prob = std::log(logits.m_vector[i].m_log_prob);
-        }
-    }
-};
-
-
 } // namespace LogitTransformers
 
 class LogitProcessor {
@@ -366,7 +353,6 @@ public:
                 if (sampling_params.top_k > 0 && sampling_params.top_k < std::numeric_limits<size_t>::max()) {
                     m_logit_transformers.emplace_back(new LogitTransformers::TopKFilter(sampling_params.top_k));
                 }
-                m_logit_transformers.emplace_back(new LogitTransformers::LogFilter());
             }
         }
     }

src/cpp/src/sampler.hpp
@@ -383,10 +383,13 @@ class Sampler {
         std::vector<Token> out_tokens;
         for (size_t token_idx = 0; token_idx < num_tokens_per_sequence; ++token_idx) {
             size_t element_to_pick = dist(rng_engine);
-            if (logits.is_vector_initialized())
-                out_tokens.push_back(logits.m_vector[element_to_pick]);
+            if (logits.is_vector_initialized()) {
+                auto logit = logits.m_vector[element_to_pick];
+                logit.m_log_prob = std::log(logit.m_log_prob);
+                out_tokens.push_back(logit);
+            }
             else
-                out_tokens.emplace_back(logits.m_data[element_to_pick], element_to_pick);
+                out_tokens.emplace_back(std::log(logits.m_data[element_to_pick]), element_to_pick);
         }
         return out_tokens;
     }

tests/python_tests/test_sampling.py
@@ -21,7 +21,8 @@ from common import run_test_pipeline, get_models_list, get_model_and_tokenizer, 
     get_beam_search, get_beam_search_min_and_max_tokens, get_beam_search_with_single_stop_string, \
     get_beam_search_with_multiple_stop_strings, get_beam_search_with_multiple_stop_strings_no_match, get_multinomial_max_and_min_token, \
     get_multinomial_temperature_and_frequence_penalty, get_multinomial_temperature_and_presence_penalty, \
-    generate_and_compare_with_hf, get_multinomial_temperature_and_repetition_penalty, get_scheduler_config
+    generate_and_compare_with_hf, get_multinomial_temperature_and_repetition_penalty, get_scheduler_config, \
+    run_continuous_batching
 
 
 @pytest.mark.precommit
@@ -290,7 +291,6 @@ RANDOM_SAMPLING_TEST_CASES = [
 
 
 @pytest.mark.precommit
-@pytest.mark.skip(reason="Random sampling results are non deterministic due to: discrete_distribution impl depends on platform, model inference results may depend on CPU. Test passes on CI but fails locally.")
 @pytest.mark.parametrize("test_struct", RANDOM_SAMPLING_TEST_CASES,
         ids=["multinomial_temperature",
              "multinomial_temperature_and_top_p",
@@ -315,7 +315,11 @@ def test_individual_generation_configs_random(tmp_path, test_struct: RandomSampl
     model_path : Path = tmp_path / model_id
     save_ov_model_from_optimum(model, hf_tokenizer, model_path)
 
-    generate_and_compare_with_reference_text(model_path, prompts, test_struct.ref_texts, generation_configs, DEFAULT_SCHEDULER_CONFIG)
+    # run multinomial without comparison with reference
+    _ = run_continuous_batching(model_path, DEFAULT_SCHEDULER_CONFIG, prompts, generation_configs)
+
+    # Reference comparison is not performed as sampling results are non-deterministic.
+    # Discrete_distribution impl depends on platform, model inference results may depend on CPU.
 
 
 