JohnLeFeng/openvino.genai

commit 52e156d0800afd823d5594d021c50976bc139d0c

Tune automatic memory allocation (#2697)
1. Clear KV-cache at the end of generate().
2. Update KV-cache growth formula to align with GPU.

Ticket: CVS-162894

---------

Co-authored-by: Alexander Suvorov <alexander.suvorov@intel.com>
Co-authored-by: Copilot <175728472+Copilot@users.noreply.github.com>

src/cpp/src/continuous_batching/cache_manager.hpp
@@ -129,7 +129,7 @@ public:
                 ov::Tensor key_cache = m_context.create_tensor(get_key_cache_precision(decoder_layer_id), key_cache_shape);
                 ov::Tensor value_cache = m_context.create_tensor(get_value_cache_precision(decoder_layer_id), value_cache_shape);
 
-                if (m_key_cache.size() > decoder_layer_id) {
+                if (m_key_cache.size() > decoder_layer_id && m_key_cache[decoder_layer_id]) {
                     ov::Coordinate end_key = m_key_cache[decoder_layer_id].get_shape();
                     ov::Coordinate end_value = m_value_cache[decoder_layer_id].get_shape();
 
@@ -164,7 +164,7 @@ public:
                 size_t key_roi_size_byte = 0;
                 size_t value_roi_size_byte = 0;
 
-                if (m_key_cache.size() > decoder_layer_id) {
+                if (m_key_cache.size() > decoder_layer_id && m_key_cache[decoder_layer_id]) {
                     ov::Coordinate end_key = m_key_cache[decoder_layer_id].get_shape();
                     ov::Coordinate end_value = m_value_cache[decoder_layer_id].get_shape();
                     // copy current cache data
@@ -276,6 +276,14 @@ public:
                 }
             }
         }
+    }
+
+    void clear() {
+        for (size_t decoder_layer_id = 0; decoder_layer_id < m_num_decoder_layers; ++decoder_layer_id) {
+            m_key_cache[decoder_layer_id] = ov::Tensor();
+            m_value_cache[decoder_layer_id] = ov::Tensor();
+        }
+        m_num_allocated_kv_blocks = 0;
     }
 };
 

src/cpp/src/continuous_batching/block_manager.hpp
@@ -177,6 +177,10 @@ class OverwritableBlocksHashStore {
         }
         return retval;
     }
+
+    void clear() {
+        m_blocks.clear();
+    }
 };
 
 class CacheStateDumper;
@@ -489,6 +493,15 @@ public:
      */
     size_t get_total_number_of_kv_blocks() const {
         return m_total_num_blocks;
+    }
+
+    void clear() {
+        m_total_num_blocks = 0;
+        m_free_blocks_num = std::vector<size_t>(m_num_layers, 0);
+        for (auto& per_layer_block_list : m_free_blocks) {
+            per_layer_block_list.clear();
+        }
+        m_overwriteable_blocks.clear();
     }
 };
 
@@ -1137,6 +1150,17 @@ public:
                 break;
             }
         }
+    }
+
+    void clear() {
+        // KV-cache should not be cleared if prefix caching is enabled
+        OPENVINO_ASSERT(m_enable_prefix_caching == false);
+
+        m_allocator.clear();
+        m_prefix_hash_to_occupied_block_map.clear();
+
+        // Block tables should be cleared when generation is finished
+        OPENVINO_ASSERT(m_block_table.empty());
     }
 };
 

src/cpp/src/continuous_batching/pipeline_impl.cpp
@@ -520,6 +520,12 @@ ContinuousBatchingPipeline::ContinuousBatchingImpl::generate(const std::vector<o
     OPENVINO_ASSERT(results.size() == input_ids.size());
 
     generate_timer.end();
+    
+    const auto& scheduler_config = m_scheduler->get_config();
+    // Clear KV-cache in case of dynamic cache allocation and no prefix caching
+    if (!scheduler_config.enable_prefix_caching && scheduler_config.cache_size == 0 && scheduler_config.num_kv_blocks == 0) {
+        m_scheduler->clear_kv_cache();
+    }
     return results;
 }
 

src/cpp/src/continuous_batching/scheduler.hpp
@@ -29,7 +29,7 @@ class Scheduler {
 
     // Dynamic KV-cache allocation params
     size_t m_kv_blocks_initial_multiplier = 2;
-    const float m_cache_growth_factor = 2; // commmon values 1.5 or 2
+    const float m_cache_growth_num_tokens = 256; // Number of tokens by which KV-cache is increased
 
     std::shared_ptr<CacheManager> m_cache_manager;
 
@@ -160,6 +160,12 @@ public:
 
     void free_blocks_from_sequence(size_t seq_id, const std::vector<std::set<size_t>>& per_layer_logical_block_indices_to_free) {
         m_block_manager->free_blocks_from_sequence(seq_id, per_layer_logical_block_indices_to_free);
+    }
+
+    void clear_kv_cache() {
+        OPENVINO_ASSERT(m_config.enable_prefix_caching == false, "KV-cache should not be cleared if prefix caching is enabled.");
+        m_cache_manager->clear();
+        m_block_manager->clear();
     }
 
 private:
@@ -566,7 +572,7 @@ private:
         }
         auto device = m_cache_manager->get_device();
         size_t current_num_of_kv_blocks = m_block_manager->get_total_number_of_kv_blocks();
-        size_t new_blocks_num = current_num_of_kv_blocks * m_cache_growth_factor;
+        size_t new_blocks_num = current_num_of_kv_blocks + std::ceil(m_cache_growth_num_tokens / get_block_size());
 
         if (device.find("GPU") == std::string::npos) {
             m_block_manager->increase_kv_blocks_number(new_blocks_num);

commit 429dc15bab9344c91ca36ff32dec7336db74f9f9

Fixed clearing of kv-cache for GPU (#2717)
Fixed regression after
https://github.com/openvinotoolkit/openvino.genai/pull/2697
Ticket: CVS-173356

Co-authored-by: Your Name <you@example.com>

src/cpp/src/continuous_batching/cache_manager.hpp
@@ -138,7 +138,10 @@ public:
                         ov::RemoteTensor dst_value_roi(value_cache, start_value, end_value);
                         dst_key_roi.copy_from(m_key_cache[decoder_layer_id]);
                         dst_value_roi.copy_from(m_value_cache[decoder_layer_id]);
+                    }
 
+                    // set new cache tensors
+                    if (m_key_cache.size() > decoder_layer_id) {
                         m_key_cache[decoder_layer_id] = key_cache;
                         m_value_cache[decoder_layer_id] = value_cache;
                     } else {