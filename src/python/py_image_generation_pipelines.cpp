01e4471cf42490effd5a28f50fd6211666358fbb
John Feng <john.feng@intel.com>
2026-07-29 01:16:26 -0800

    add python support

--- src/python/py_image_generation_pipelines.cpp
+++ src/python/py_image_generation_pipelines.cpp
@@ -355,6 +355,16 @@ void init_image_generation_pipelines(py::module_& m) {
         .def("to_string", &ov::genai::TaylorSeerCacheConfig::to_string)
         .def("__repr__", &ov::genai::TaylorSeerCacheConfig::to_string);
 
+    py::enum_<ov::genai::InpaintingMode>(m, "InpaintingMode")
+        .value("STANDARD", ov::genai::InpaintingMode::STANDARD)
+        .value("ATTENTIVE_ERASER", ov::genai::InpaintingMode::ATTENTIVE_ERASER);
+
+    py::class_<ov::genai::AttentiveEraserConfig>(m, "AttentiveEraserConfig")
+        .def(py::init<>())
+        .def_readwrite("rm_guidance_scale", &ov::genai::AttentiveEraserConfig::rm_guidance_scale)
+        .def_readwrite("ss_steps", &ov::genai::AttentiveEraserConfig::ss_steps)
+        .def("validate", &ov::genai::AttentiveEraserConfig::validate);
+
     py::class_<ov::genai::ImageGenerationConfig>(m, "ImageGenerationConfig", "This class is used for storing generation config for image generation pipeline.")
         .def(py::init<>())
         .def_readwrite("prompt_2", &ov::genai::ImageGenerationConfig::prompt_2)
@@ -373,6 +383,7 @@ void init_image_generation_pipelines(py::module_& m) {
         .def_readwrite("strength", &ov::genai::ImageGenerationConfig::strength)
         .def_readwrite("max_sequence_length", &ov::genai::ImageGenerationConfig::max_sequence_length)
         .def_readwrite("taylorseer_config", &ov::genai::ImageGenerationConfig::taylorseer_config)
+        .def_readwrite("attentive_eraser", &ov::genai::ImageGenerationConfig::attentive_eraser)
         .def("validate", &ov::genai::ImageGenerationConfig::validate)
         .def("update_generation_config", [](
             ov::genai::ImageGenerationConfig& config,
@@ -653,7 +664,9 @@ void init_image_generation_pipelines(py::module_& m) {
             const py::kwargs& kwargs
         ) {
             ScopedVar env_manager(pyutils::ov_tokenizers_module_path());
-            return std::make_unique<ov::genai::InpaintingPipeline>(models_path, device, pyutils::kwargs_to_any_map(kwargs));
+            return std::make_unique<ov::genai::InpaintingPipeline>(models_path,
+                                                                   device,
+                                                                   pyutils::kwargs_to_any_map(kwargs));
         }),
         py::arg("models_path"), "folder with exported model files.",
         py::arg("device"), "device on which inference will be done",
@@ -666,6 +679,7 @@ void init_image_generation_pipelines(py::module_& m) {
         .def("get_generation_config", &ov::genai::InpaintingPipeline::get_generation_config, py::return_value_policy::copy)
         .def("set_generation_config", &ov::genai::InpaintingPipeline::set_generation_config, py::arg("config"))
         .def("set_scheduler", &ov::genai::InpaintingPipeline::set_scheduler, py::arg("scheduler"))
+        .def("set_attentive_eraser_mode", &ov::genai::InpaintingPipeline::set_attentive_eraser_mode, py::arg("enable"), "Enable or disable Attentive Eraser mode for inpainting")
         .def("reshape", &ov::genai::InpaintingPipeline::reshape, py::arg("num_images_per_prompt"), py::arg("height"), py::arg("width"), py::arg("guidance_scale"))
         .def_static("stable_diffusion", &ov::genai::InpaintingPipeline::stable_diffusion, py::arg("scheduler"), py::arg("clip_text_model"), py::arg("unet"), py::arg("vae"))
         .def_static("latent_consistency_model", &ov::genai::InpaintingPipeline::latent_consistency_model, py::arg("scheduler"), py::arg("clip_text_model"), py::arg("unet"), py::arg("vae"))
