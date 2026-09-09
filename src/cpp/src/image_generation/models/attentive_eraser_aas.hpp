fa90d615ecb9f60d18f36bab4c4b9a4383aa9e54
John Feng <john.feng@intel.com>
2026-09-09 09:23:11 -0800

    add AAS layer

---
+++ src/cpp/src/image_generation/models/attentive_eraser_aas.hpp
@@ -0,0 +1,20 @@
+// Copyright (C) 2023-2026 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "openvino/core/model.hpp"
+
+namespace ov {
+namespace genai {
+
+inline constexpr const char* ATTENTIVE_ERASER_AAS_LAYERS = "ATTENTIVE_ERASER_AAS_LAYERS";
+
+void apply_attentive_eraser_aas(const std::shared_ptr<ov::Model>& model,
+                                const std::vector<size_t>& layer_indices);
+
+}  // namespace genai
+}  // namespace ov
